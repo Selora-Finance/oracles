50f5432dba253faeb0feadfce018001702d6d40b
Kingsley Victor <javaprodigy56@gmail.com>
2025-06-28 05:43:33 +0100

    chore: deploy new oracle

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -9,7 +9,7 @@ const config: HardhatUserConfig = {
   solidity: '0.8.28',
   networks: {
     monadTestnet: {
-      url: 'https://10143.rpc.thirdweb.com/f4f634aad888c9e6d43dd22ed5d02158',
+      url: 'https://testnet-rpc.monad.xyz',
       chainId: 10143,
       accounts: [process.env.PRIVATE_KEY as string],
       gasPrice: 'auto',
